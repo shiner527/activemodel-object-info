b5f04a332caa8e0aefa1f06d89b385fe7785ef4e
shiner527 <shiner527@hotmail.com>
2021-05-19 14:42:48 +0800

    Change table definition

--- lib/activemodel_object_info.rb
+++ lib/activemodel_object_info.rb
@@ -3,8 +3,13 @@
 require 'activemodel_object_info/extend'
 require 'activemodel_object_info/version'
 require 'activemodel_object_info/base'
+require 'activemodel_object_info/table_definition'
 require 'activemodel_object_info/deleted_operation'
 
+#
+# 扩展模组。
+#
+# @author shiner527 <shiner527@hotmail.com>
+#
 module ActivemodelObjectInfo
-  class Error < StandardError; end
 end
